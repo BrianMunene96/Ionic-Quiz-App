b04406fd1f2c8ac64292972d9d488a613721158d
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-03-02 21:09:10 +0100

    remove unnecessary files

--- src/assets/questions.js
+++ src/assets/questions.js
@@ -1,104 +1,47 @@
 export const questions = [
   {
-    text: 'Which of the following is correct about CSS?',
+    text: 'What is the capital of America?',
     options: [
-      {
-        id: 0,
-        text: 'Style sheets allow content to be optimized for more than one type of device.',
-        isCorrect: false,
-      },
-      {
-        id: 1,
-        text: 'CSS can store web applications locally with the help of an offline catche',
-        isCorrect: false,
-      },
-      {
-        id: 2,
-        text: 'Using CSS, we can view offline websites.The cache also ensures faster loading and better overall performance of the website',
-        isCorrect: false,
-      },
-      { id: 3, text: 'All of the above', isCorrect: true },
+      { id: 0, text: 'New York City', isCorrect: false },
+      { id: 1, text: 'Boston', isCorrect: false },
+      { id: 2, text: 'Santa Fe', isCorrect: false },
+      { id: 3, text: 'Washington DC', isCorrect: true },
     ],
   },
   {
-    text: 'Which of the following defines a measurement in inches?',
+    text: 'What year was the Constitution of America written?',
     options: [
-      { id: 0, text: 'in', isCorrect: true },
-      { id: 1, text: 'mm', isCorrect: false },
-      { id: 2, text: 'pc', isCorrect: false },
-      { id: 3, text: 'pt', isCorrect: false },
+      { id: 0, text: '1787', isCorrect: true },
+      { id: 1, text: '1776', isCorrect: false },
+      { id: 2, text: '1774', isCorrect: false },
+      { id: 3, text: '1826', isCorrect: false },
     ],
   },
   {
-    text: 'Which of the following defines 1% of viewport height?',
+    text: 'Who was the second president of the US?',
     options: [
-      { id: 0, text: 'px', isCorrect: false },
-      { id: 1, text: 'vh', isCorrect: true },
-      { id: 2, text: 'vw', isCorrect: false },
-      { id: 3, text: 'vmin', isCorrect: false },
+      { id: 0, text: 'John Adams', isCorrect: true },
+      { id: 1, text: 'Paul Revere', isCorrect: false },
+      { id: 2, text: 'Thomas Jefferson', isCorrect: false },
+      { id: 3, text: 'Benjamin Franklin', isCorrect: false },
     ],
   },
   {
-    text: 'Which of the following property is used to set the background color of an element?',
+    text: 'What is the largest state in the US?',
     options: [
-      { id: 0, text: 'background-color', isCorrect: true },
-      { id: 1, text: 'background-image', isCorrect: false },
-      { id: 2, text: 'background-repeat', isCorrect: false },
-      { id: 3, text: 'background-position', isCorrect: false },
+      { id: 0, text: 'California', isCorrect: false },
+      { id: 1, text: 'Alaska', isCorrect: true },
+      { id: 2, text: 'Texas', isCorrect: false },
+      { id: 3, text: 'Montana', isCorrect: false },
     ],
   },
   {
-    text: 'Which of the following property is used to set the color of a text?',
+    text: 'Which of the following countries DO NOT border the US?',
     options: [
-      { id: 0, text: 'color', isCorrect: true },
-      { id: 1, text: 'direction', isCorrect: false },
-      { id: 2, text: 'letter-spacing', isCorrect: false },
-      { id: 3, text: 'word-spacing', isCorrect: false },
-    ],
-  },
-  {
-    text: 'Which of the following property is used to control the flow and formatting of text?',
-    options: [
-      { id: 0, text: 'white-space', isCorrect: true },
-      { id: 1, text: 'text-shadow', isCorrect: false },
-      { id: 2, text: 'text-decoration', isCorrect: false },
-      { id: 3, text: 'text-transform', isCorrect: false },
-    ],
-  },
-  {
-    text: 'Which of the following property specifies the color of a border?',
-    options: [
-      { id: 0, text: ':border-color', isCorrect: true },
-      { id: 1, text: ':border-style', isCorrect: false },
-      { id: 2, text: ':border-width', isCorrect: false },
-      { id: 3, text: ':border-bottom-color', isCorrect: false },
-    ],
-  },
-  {
-    text: 'Which of the following property changes the style of top border?',
-    options: [
-      { id: 0, text: ':border-bottom-style', isCorrect: false },
-      { id: 1, text: ':border-top-style', isCorrect: true },
-      { id: 2, text: ':border-left-style', isCorrect: false },
-      { id: 3, text: ':border-right-style', isCorrect: false },
-    ],
-  },
-  {
-    text: 'Which of the following property specifies an image for the marker rather than a bullet-point or number?',
-    options: [
-      { id: 0, text: 'list-style-type', isCorrect: false },
-      { id: 1, text: 'list-style-position', isCorrect: true },
-      { id: 2, text: 'list-style-image', isCorrect: true },
-      { id: 3, text: 'list-style', isCorrect: false },
-    ],
-  },
-  {
-    text: 'Which of the following value of cursor shows it as a pointing hand?',
-    options: [
-      { id: 0, text: 'crosshair', isCorrect: false },
-      { id: 1, text: 'default', isCorrect: false },
-      { id: 2, text: 'pointer', isCorrect: true },
-      { id: 3, text: 'move', isCorrect: false },
+      { id: 0, text: 'Canada', isCorrect: false },
+      { id: 1, text: 'Russia', isCorrect: true },
+      { id: 2, text: 'Cuba', isCorrect: true },
+      { id: 3, text: 'Mexico', isCorrect: false },
     ],
   },
 ];
